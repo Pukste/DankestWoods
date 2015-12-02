4fcd3dd722e29cecb6ff3f3475d7d3d553866bac
Pukste <ville_pukki@hotmail.com>
2015-12-02 08:02:32 +0200

    testiä

--- dialog.py
+++ dialog.py
@@ -1,10 +1,10 @@
 # testi dialog treetä varten jos npc jne..
 
-
-print("")
-print("")
-print("")
-print("")
+conta = 0
+print("abc?")
+print("a")
+print("b")
+print("c")
 while True:
     answer = input()
     answer = answer.lower()
@@ -12,45 +12,9 @@ while True:
         break
     print("Please input a, b, c or d.")
 if answer == 'a':
-    print("a")
-    print("")
-    print("")
-    while True:
-        answer = input()
-        answer = answer.lower()
-        if answer in ['a', 'b', 'c', 'd', 'quit']:
-            break
-        print("Please input a, b, or c.")
-    if answer == 'a':
-        print("b")
-        input("blabla")
-    elif answer == 'b':
-        print("c")
-        input("blabla")
-    else:
-        print("")
-elif answer == 'b':
+    print("a:bc?")
     print("b")
-    print("")
-    print("")
-    while True:
-        answer = input()
-        answer = answer.lower()
-        if answer in ['a', 'b', 'c', 'd', 'quit']:
-            break
-        print("Please input a, b, or c.")
-    if answer == 'a':
-        print("a")
-        input("blabla")
-    elif answer == 'b':
-        print("c")
-        input("blabla")
-    else:
-        print("")
-elif answer == 'c':
     print("c")
-    print("")
-    print("")
     while True:
         answer = input()
         answer = answer.lower()
@@ -58,12 +22,125 @@ elif answer == 'c':
             break
         print("Please input a, b, or c.")
     if answer == 'a':
-        print("a")
-        input("blabla")
-    elif answer == 'b':
         print("b")
         input("blabla")
-    else:
-        print("")
+    if answer == 'b':
+        print("c")
+        input("blabla")
+    if answer in ['c', 'd', 'quit']:
+        print("return")  # will be replaced by return
+    conta = 1
+elif answer == 'b':
+    print("b:ac?")
+    print("a")
+    print("c")
+    while True:
+        answer = input()
+        answer = answer.lower()
+        if answer in ['a', 'b', 'c', 'd', 'quit']:
+            break
+        print("Please input a, b, or c.")
+    if answer == 'a':
+        print("a")
+        input("blabla")
+    if answer == 'b':
+        print("c")
+        input("blabla")
+    if answer in ['c', 'd', 'quit']:
+        print("return")  # will be replaced by return
+    conta = 1
+elif answer == 'c':
+    print("c:ab?")
+    print("a")
+    print("b")
+    while True:
+        answer = input()
+        answer = answer.lower()
+        if answer in ['a', 'b', 'c', 'd', 'quit']:
+            break
+        print("Please input a, b, or c.")
+    if answer == 'a':
+        print("a")
+        input("blabla")
+    if answer == 'b':
+        print("b")
+        input("blabla")
+    if answer in ['c', 'd', 'quit']:
+        print("return")  # will be replaced by return
+    conta = 1
 else:
     print("")
+
+if conta == 1:
+    print("abc?")
+    print("a")
+    print("b")
+    print("c")
+    while True:
+        answer = input()
+        answer = answer.lower()
+        if answer in ['a', 'b', 'c', 'd', 'quit']:
+            break
+        print("Please input a, b, c or d.")
+    if answer == 'a':
+        print("a:bc?")
+        print("b")
+        print("c")
+        while True:
+            answer = input()
+            answer = answer.lower()
+            if answer in ['a', 'b', 'c', 'd', 'quit']:
+                break
+            print("Please input a, b, or c.")
+        if answer == 'a':
+            print("b")
+            input("blabla")
+        if answer == 'b':
+            print("c")
+            input("blabla")
+        if answer in ['c', 'd', 'quit']:
+            print("return")  # will be replaced by return
+        conta = 2
+    elif answer == 'b':
+        print("b:ac?")
+        print("a")
+        print("c")
+        while True:
+            answer = input()
+            answer = answer.lower()
+            if answer in ['a', 'b', 'c', 'd', 'quit']:
+                break
+            print("Please input a, b, or c.")
+        if answer == 'a':
+            print("a")
+            input("blabla")
+        if answer == 'b':
+            print("c")
+            input("blabla")
+        if answer in ['c', 'd', 'quit']:
+            print("return")  # will be replaced by return
+        conta = 2
+    elif answer == 'c':
+        print("c:ab?")
+        print("a")
+        print("b")
+        while True:
+            answer = input()
+            answer = answer.lower()
+            if answer in ['a', 'b', 'c', 'd', 'quit']:
+                break
+            print("Please input a, b, or c.")
+        if answer == 'a':
+            print("a")
+            input("blabla")
+        if answer == 'b':
+            print("b")
+            input("blabla")
+        if answer in ['c', 'd', 'quit']:
+            print("return")  # will be replaced by return
+        conta = 2
+    else:
+        print("")
+
+if conta == 2:
+    print(conta)
