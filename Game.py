83eb4a57b6b5c9522d1016ebb49782a6d5d242ee
Pukste <ville_pukki@hotmail.com>
2015-12-10 08:25:19 +0200

    vähä enemmän viilausta

--- Game.py
+++ Game.py
@@ -420,4 +420,4 @@ while ending == 0:
     else:
         print("Invalid command...")
 
-input("The game has ended. Thank you for playing! Press enter to end the game.")
+input("Thank you for playing! Press enter to end the game.")
